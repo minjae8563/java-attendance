284b8b57df343c4e0737915d62d011a6352fde1e
(minjae8563) <(minjae8563@naver.com)>
2025-02-28 10:54:04 +0900

    refactor : 멤버 변수 상수화

--- src/main/java/util/FileInput.java
+++ src/main/java/util/FileInput.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,6 +18,9 @@ public class FileInput {
 
     private static final String FILE_PATH = "src/main/resources/attendances.csv";
     private static final String STUDENT_INFORMATION_REGEX = "[가-힣]+,\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}";
+    private static final int NAME_INDEX = 0;
+    private static final int TIME_INFORMATION_INDEX = 1;
+    private static final String LOCAL_DATE_TIME_FORMATTER = "yyyy-MM-dd HH:mm";
 
     public static Map<String, List<AttendanceDateTime>> readFileAndCreateStudentRepository(){
         Map<String, List<AttendanceDateTime>> studentInformation = new HashMap<>();
@@ -26,10 +30,9 @@ public class FileInput {
                     throw new IllegalArgumentException("[ERROR] 잘못된 파일 양식입니다.");
                 }
                 String[] nameAndTimeInformation = information.split(",");
-                String name = nameAndTimeInformation[0];
-                String timeInformation = nameAndTimeInformation[1];
-                String localDateTimeFormatter = "yyyy-MM-dd HH:mm";
-                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(localDateTimeFormatter);
+                String name = nameAndTimeInformation[NAME_INDEX];
+                String timeInformation = nameAndTimeInformation[TIME_INFORMATION_INDEX];
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(LOCAL_DATE_TIME_FORMATTER);
                 LocalDateTime localDateTime = LocalDateTime.parse(timeInformation, dateTimeFormatter);
                 studentInformation.computeIfAbsent(name, k -> new ArrayList<>()).add(new AttendanceDateTime(localDateTime));
             }
